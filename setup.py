b030842f2822782ba44488260d4f7361e2aa9194
aaronfraint <aaronfraint@icloud.com>
2021-07-14 13:52:21 -0400

    install codebase into env

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="0.0.1",
+    description="Summarize results from Google Form surveys using Python (+ pandas and xlsxwriter)",
+    author="Aaron Fraint, AICP",
+    license="GNU GPL v3",
+)
